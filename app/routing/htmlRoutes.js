871b9c4002beb078fd4f5ec3823b6d5947597653
Benny V <bengv2303@gmail.com>
2019-01-27 15:52:44 -0700

    still need to link html pages - getting error with file path & error message 'Segmenation Fault' when node server.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,10 @@
 var path = require("path");
 module.exports = function (app) {
-    app.get("/", function(request, response) {
+    app.get("/", function (request, response) {
         response.sendFile(path.join(__dirname, "/../public/home.html"))
 
     })
-    app.get("/survey", function(request, response) {
+    app.get("/survey", function (request, response) {
         response.sendFile(path.join(__dirname, "/../public/survey.html"))
 
     })
