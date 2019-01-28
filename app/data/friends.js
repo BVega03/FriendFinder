ca9b88f4d6f487a120aab79b7958fe9fbc39bac6
Benny V <bengv2303@gmail.com>
2019-01-27 17:36:22 -0700

    updated html & js files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,143 @@
-var friends = [{
-        name: "Ben",
-        age: 34
+// Apple > MS
+// PS > Xbox
+// Jordan > Kobe
+// Coke > Pepsi
+// Biggie > 2Pac
+// Veggies > Meat
+// Mtns > Beach
+// Superman > Batman
+// Animals > Most Humans
+// Party > Netflix
+
+var friends = [
+    {
+        name: "Ron",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=&url=https%3A%2F%2Ftwitter.com%2Fmagasocal&psig=AOvVaw1zCvUIm-XWg8cjeHXC452Y&ust=1548720268143789",
+        scores: [
+            5,
+            1,
+            5,
+            3,
+            1,
+            1,
+            1,
+            5,
+            5,
+            5
+        ]
     },
     {
-        name: "Jack",
-        age: 29
-    }
-]
+        name: "Gob",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi3uuvWmY_gAhUm3YMKHZY4CpgQjRx6BAgBEAU&url=http%3A%2F%2Finconvenientbluth.blogspot.com%2F2009%2F03%2Fgeorge-oscar-gob-bluth-jr.html&psig=AOvVaw2DYNspJ7OlYvHlTpI047jm&ust=1548721045742061",
+        scores: [
+            2,
+            2,
+            2,
+            2,
+            2,
+            1,
+            1,
+            1,
+            1,
+            5
+        ]
+    },
+    {
+        name: "Swanson",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjChPKumY_gAhUr3IMKHSnvB4sQjRx6BAgBEAU&url=https%3A%2F%2Fwww.charactour.com%2Fhub%2Fcharacters%2Fview%2FRon-Swanson.Parks-and-Recreation&psig=AOvVaw3-_TY08bQTUZQNgZeSFkc0&ust=1548720308716019",
+        scores: [
+            1,
+            1,
+            5,
+            3,
+            1,
+            1,
+            4,
+            4,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Dwayne",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=&url=https%3A%2F%2Fwww.imdb.com%2Fname%2Fnm0425005%2F&psig=AOvVaw0SJSHn9_293PvqvDoU1f5u&ust=1548720371875273",
+        scores: [
+            5,
+            5,
+            5,
+            3,
+            3,
+            3,
+            3,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Cardi",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjmscj_mI_gAhWg8oMKHbn5BB4QjRx6BAgBEAU&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.biography.com%252Fpeople%252Fcardi-b%26psig%3DAOvVaw3oHmBKR5DxWQGO7cSLCoRP%26ust%3D1548720490887605&psig=AOvVaw3oHmBKR5DxWQGO7cSLCoRP&ust=1548720490887605",
+        scores: [
+            3,
+            3,
+            1,
+            3,
+            5,
+            5,
+            1,
+            1,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Fey",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwitxcLOmI_gAhXFz4MKHXRSBOcQjRx6BAgBEAU&url=https%3A%2F%2Fdeadline.com%2F2016%2F02%2Ftina-fey-universal-deal-do-nothing-bitches-ronda-rousey-1201695532%2F&psig=AOvVaw0DaNt_rGOs9wStMemAAxbT&ust=1548720760362326",
+        scores: [
+            1,
+            1,
+            1,
+            3,
+            1,
+            4,
+            4,
+            5,
+            5,
+            1
+        ]
+    },
+    {
+        name: "JLo",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2F2%2F29%2FJennifer_Lopez_-_J.Lo.png%2F220px-Jennifer_Lopez_-_J.Lo.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FJ.Lo_(album)&docid=ThFiFc14X1q_JM&tbnid=Wsq0bQGy5l8ISM%3A&vet=10ahUKEwjn7LKzl4_gAhWJzIMKHQzhAoMQMwiaASggMCA..i&w=220&h=220&bih=886&biw=2000&q=Jlo&ved=0ahUKEwjn7LKzl4_gAhWJzIMKHQzhAoMQMwiaASggMCA&iact=mrc&uact=8",
+        scores: [
+            5,
+            5,
+            5,
+            3,
+            5,
+            3,
+            3,
+            1,
+            5,
+            3
+        ]
+    },
+    {
+        name: "Wonder",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj_8OrsmY_gAhWjyoMKHTVfBnoQjRx6BAgBEAU&url=https%3A%2F%2Fwww.psychologytoday.com%2Fus%2Fblog%2Fminding-memory%2F201706%2Fwonder-woman-how-i-wish&psig=AOvVaw3A3xqbCLp9rDQoMxVb9bsN&ust=1548720036855019",
+        scores: [
+            3,
+            5,
+            5,
+            5,
+            5,
+            4,
+            4,
+            3,
+            5,
+            1
+        ]
+    },
+];
 
 module.exports = friends;
